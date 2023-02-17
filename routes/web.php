2d3d76cfbed432bba3d0e7b30a81d353f5f70f10
Noiem Uddin <smnoiem@gmail.com>
2023-02-17 17:31:33 +0600

    removed unnecessary services from docker compose

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,26 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::name('public')->group(function () {
 
-Route::get('/', function () {
-    return view('home'); // Provides links for other public routes
+    Route::get('/', function () {
+        return view('home'); // Provides links for other public routes
+    });
+
+    Route::get('/registration', [RegistrationController::class, 'create'])->name('registration.create');
+
+    Route::post('/registration', [RegistrationController::class, 'store'])->name('registration.store');
+
+    Route::get('/vaccine-status', [VaccineStatusController::class, 'showForm'])->name('vaccine.status');
+
+    Route::post('/vaccine-status', [VaccineStatusController::class, 'showVaccineStatus'])->name('vaccine.status');
+
+    Route::get('/vaccine-card', [VaccineCardController::class, 'showForm'])->name('vaccine.card');
+
+    Route::post('/vaccine-card', [VaccineCardController::class, 'generateVaccineCard'])->name('vaccine.card');
+
+    Route::get('/vaccine-certificate', [VaccineCertificateController::class, 'showForm'])->name('vaccine.card');
+
+    Route::post('/vaccine-certificate', [VaccineCertificateController::class, 'generateCertificate'])->name('vaccine.card');
+
 });
-
-Route::get('/registration', [RegistrationController::class, 'create'])->name('registration.create');
-
-Route::post('/registration', [RegistrationController::class, 'store'])->name('registration.store');
-
-Route::get('/vaccine-status', [VaccineStatusController::class, 'showForm'])->name('vaccine.status');
-
-Route::post('/vaccine-status', [VaccineStatusController::class, 'showVaccineStatus'])->name('vaccine.status');
-
-Route::get('/vaccine-card', [VaccineCardController::class, 'showForm'])->name('vaccine.card');
-
-Route::post('/vaccine-card', [VaccineCardController::class, 'generateVaccineCard'])->name('vaccine.card');
-
-Route::get('/vaccine-certificate', [VaccineCertificateController::class, 'showForm'])->name('vaccine.card');
-
-Route::post('/vaccine-certificate', [VaccineCertificateController::class, 'generateCertificate'])->name('vaccine.card');
